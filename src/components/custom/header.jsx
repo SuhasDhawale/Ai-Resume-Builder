a210f8f34071dd9428e59e499d9993832e93315a
SuhasDhawale <suhasdhawale57@gmail.com>
2024-08-17 15:47:03 +0530

    linked signIn to Get started btn

--- src/components/custom/header.jsx
+++ src/components/custom/header.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
+import { UserButton, useUser } from '@clerk/clerk-react'
 
 function Header() {
+    const {user, isSignedIn}=useUser();
   return (
     <div className='p-3 px-5 flex justify-between shadow-md'>
       <img src="/logo.svg" width={100}  height={100}/>
 
-      <Button>Get Started</Button>
+      {
+        isSignedIn?
+        <div className='flex gap-2 items-center'>
+          <Button variant="outline">Dashboard</Button>
+        <UserButton/>
+        </div>:
+        <Link to={'/auth/sign-in'}>
+        <Button>Get Started</Button>
+      </Link>
+      }
+      
     </div>
   )
 }
