ad9dd9507eca7219aeb1f17c1e4a2ccf8f6db625
SuhasDhawale <suhasdhawale57@gmail.com>
2024-08-17 15:37:45 +0530

    fixed get started button position

--- src/components/custom/header.jsx
+++ src/components/custom/header.jsx
@@ -3,7 +3,7 @@ import { Button } from '../ui/button'
 
 function Header() {
   return (
-    <div className='p-3 px-5'>
+    <div className='p-3 px-5 flex justify-between shadow-md'>
       <img src="/logo.svg" width={100}  height={100}/>
 
       <Button>Get Started</Button>
