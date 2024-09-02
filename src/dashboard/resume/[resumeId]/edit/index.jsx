8df964bbe8f0f314bcace3c29e8a6e28a8e73026
SuhasDhawale <suhasdhawale57@gmail.com>
2024-09-02 20:24:54 +0530

    added a dummy Resume for Preview

--- src/dashboard/resume/[resumeId]/edit/index.jsx
+++ src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import FormSection from '../../components/FormSection';
 import ResumePreview from '../../components/ResumePreview';
+import { ResumeInfoContext } from '@/context/ResumeInfoContext';
+import dummy from '@/data/dummy';
 
 function EditResume() {
     const params=useParams();
+    const [resumeInfo, setResumeInfo]=useState(dummy);
 
     useEffect(()=>{
         console.log(params.resumeId)
     },[])
   return (
+     <ResumeInfoContext.Provider value={{resumeInfo, setResumeInfo}}>
     <div className='grid grid-col-1 md:grid-cols-2 p-10 gap-10'>
       {/*Form Section */}
       <FormSection/>
@@ -17,6 +21,7 @@ function EditResume() {
       <ResumePreview/>
 
     </div>
+    </ResumeInfoContext.Provider>
   )
 }
 
